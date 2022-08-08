5f496d7626ba6ed87b1278490e6bb469d0e79a74
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-08-08 01:00:49 +0100

    updating toggleNav

--- script.js
+++ script.js
@@ -14,9 +14,11 @@ const toggleNav = () => {
     overlay.classList.toggle('overlay-active');
     if (overlay.classList.contains('overlay-active')) {
         // Animate In - Overlay
+        overlay.classList.remove('overlay-slide-left');
         overlay.classList.add('overlay-slide-right');
     } else {
         // Animate out - Overlay
+        overlay.classList.remove('overlay-slide-right');
         overlay.classList.add('overlay-slide-left');
     };
 }
